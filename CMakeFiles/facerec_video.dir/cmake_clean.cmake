878deffd7d289e9cd4081949e1c4a1b31bf8ab58
JoshVorick <josh.vorick@gmail.com>
2014-09-19 22:32:19 -0500

    Got demo working

---
+++ CMakeFiles/facerec_video.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/facerec_video.dir/facerec_video.cpp.o"
+  "facerec_video.pdb"
+  "facerec_video"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/facerec_video.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
